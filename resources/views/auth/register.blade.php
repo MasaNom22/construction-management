39ad0bb572e789c59cb5f33b5153f7eaa56bf8fe
masakatsu.nomura <masakatsunomura26@gmail.com>
2020-12-30 05:37:00 +0000

    registration

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,36 @@
+@extends('app')
+
+@section('content')
+    <div class="text-center">
+        <h1>ユーザー新規登録</h1>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6 offset-sm-3">
+
+            {!! Form::open(['route' => 'signup.post']) !!}
+                <div class="form-group">
+                    {!! Form::label('name', '名前') !!}
+                    {!! Form::text('name', old('name'), ['class' => 'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('email', 'メールアドレス') !!}
+                    {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('password', 'パスワード') !!}
+                    {!! Form::password('password', ['class' => 'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('password_confirmation', 'パスワード確認用') !!}
+                    {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
+                </div>
+
+                {!! Form::submit('新規登録', ['class' => 'btn  blue-gradient btn-block']) !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
+@endsection
---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,20 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand-sm navbar-dark blue-gradient">
+        {{-- トップページへのリンク --}}
+        <a class="navbar-brand" href="/"><i class="fas fa-archway mr-1"></i>Construction-management</a>
+
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                {{-- ユーザ登録ページへのリンク --}}
+                <li>{!! link_to_route('signup.get', '新規登録', [], ['class' => 'nav-link']) !!}</li>
+                {{-- ログインページへのリンク --}}
+                <li><a href="#">ログイン</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
