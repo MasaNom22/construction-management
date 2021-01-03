61ff8a525d6a51d0c7f377dea4ebe2164b718cf9
masakatsu.nomura <masakatsunomura26@gmail.com>
2021-01-03 09:22:53 +0000

    トップページデザイン変更

--- resources/views/image_list.blade.php
+++ resources/views/image_list.blade.php
@@ -4,21 +4,22 @@
 
 @section('content')
 
-<hr />
 <div class=container>
     <div class=row>
         @foreach($images as $image)
-        <div style="float:left;" class="col-md-6">
-            <p>{{ $image->title }}</p>
-	        <p>{{ $image->content }}</p>
+        <div style="" class="col-md-6 mt-4 mb-4">
+            <h4>現場名: {{ $image->title }}</h4>
+	        <h5>リフォーム内容: {{ $image->content }}</h5>
 	                    {{-- タスクリストへのリンク --}}
-            <a class="" href="{{ route('tasks.index',$image->id) }}">タスク投稿</a>
+            <a class="" href="{{ route('tasks.index',$image->id) }}">タスク一覧画面へ</a>
+            <div class="mx-2">
 	        <img src="{{ Storage::url($image->file_path) }}" style="width:100%;"　alt="建設現場の写真"/>
+	        </div>
 	        <!--<p>{{ $image->file_name }}</p>-->
 
 	        {{-- メッセージ削除フォーム --}}
             {!! Form::model($image, ['route' => ['delete_image', $image->id], 'method' => 'delete']) !!}
-                {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+                {!! Form::submit('画像削除', ['class' => 'btn btn-danger']) !!}
             {!! Form::close() !!}
         </div>
         @endforeach
