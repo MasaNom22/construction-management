535fa8730cad94dee53e537dab4e8827a542a76c
masakatsu.nomura <masakatsunomura26@gmail.com>
2021-01-05 12:17:32 +0000

    画像投稿画面修正

--- resources/views/upload_form.blade.php
+++ resources/views/upload_form.blade.php
@@ -13,7 +13,8 @@
 	</ul>
 </div>
 @endif
-<form 	method="post" action="{{ route('upload_image') }}" enctype="multipart/form-data">
+<h2>現場画像登録画面</h2>
+<form method="post" action="{{ route('upload_image') }}" enctype="multipart/form-data">
 
 	@csrf
 	<div class="form-group row">
@@ -28,9 +29,18 @@
 			<input type="text" name="content" id="content" />
 		</div>
 	</div>
-	<input type="file" name="image" accept="image/png, image/jpeg" />
-
-	<input type="submit" value="建築現場の写真を保存する">
+	<div class="form-group row">
+		<label for="content" class="col-md-4 col-form-label text-md-right fas fa-arrow-alt-circle-down">{{ __('画像を選択') }}</label>
+		<div class="col-md-6">
+			<input type="file" name="image" accept="image/png, image/jpeg" />
+		</div>
+	</div>
+	<div class="form-group row">
+		<label for="content" class="col-md-4 col-form-label text-md-right">{{ __('') }}</label>
+		<div class="col-md-6">
+			<input type="submit" value="建築現場の写真を保存する">
+		</div>
+	</div>
 </form>
 
 @endsection
