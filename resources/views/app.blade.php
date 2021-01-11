67b70b3cfbb9136164f5894e70305077aa48bd02
masakatsu.nomura <masakatsunomura26@gmail.com>
2021-01-11 09:11:52 +0000

    タスク一覧画面修正

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -64,5 +64,5 @@
             <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/js/mdb.min.js"></script>
     </body>
     {{-- ナビゲーションバー --}}
-        @include('footer')
+        <!--@include('footer')-->
 </html>
