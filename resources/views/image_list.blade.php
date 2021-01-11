7f6880a5ca1a67294613694126ed8ca6da6507ed
masakatsu.nomura <masakatsunomura26@gmail.com>
2021-01-11 10:11:07 +0000

    削除ボタン修正

--- resources/views/image_list.blade.php
+++ resources/views/image_list.blade.php
@@ -24,7 +24,7 @@
                     <div class="d-inline-flex col-md-6">
         	        {{-- メッセージ削除フォーム --}}
                     {!! Form::model($image, ['route' => ['delete_image', $image->id], 'method' => 'delete']) !!}
-                        {!! Form::submit('削除', ['class' => 'btn btn-success']) !!}
+                        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
                     {!! Form::close() !!}
                     </div>
                 </div>
