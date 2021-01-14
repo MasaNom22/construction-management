e515d97e198e31d5f1cac6830899d14ce0ad0150
masakatsu.nomura <masakatsunomura26@gmail.com>
2021-01-14 13:05:24 +0000

    チャット機能追加

---
+++ app/Http/Controllers/ChatsController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Comment;
+use Illuminate\Support\Facades\Auth;
+
+class ChatsController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $comments = Comment::get();
+        return view('chats/index', ['comments' => $comments]);
+    }
+    
+    public function add(Request $request)
+{
+    $user = Auth::user();
+    $comment = $request->input('comment');
+    Comment::create([
+        'user_id' => $user->id,
+        'name' => $user->name,
+        'comment' => $comment
+    ]);
+    return redirect()->route('chats.index');
+}
+
+    public function getData()
+{
+    $comments = Comment::orderBy('created_at', 'desc')->get();
+    $json = ["comments" => $comments];
+    return response()->json($json);
+}
+}
