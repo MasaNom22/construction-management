1dac6de686ce5f35f01edd1e9177aa402e520813
masakatsu.nomura <masakatsunomura26@gmail.com>
2021-01-17 02:37:31 +0000

    インデント修正

--- resources/views/image_edit.blade.php
+++ resources/views/image_edit.blade.php
@@ -11,24 +11,24 @@
                 method="POST"
         >
         @csrf
-              <div class="form-group">
+            <div class="form-group">
                 <label for="title">現場名</label>
                 <input type="text" class="form-control" name="title" id="title"
                        value="{{ old('title') ?? $image->title }}" />
-              </div>
-             <div class="form-group">
+            </div>
+            <div class="form-group">
                 <label for="title">施工内容</label>
                 <input type="text" class="form-control" name="content" id="content"
                        value="{{ old('content') ?? $image->content }}" />
-              </div>
+             </div>
 	        <div class="text-right">
                 <button type="submit" class="btn btn-primary">変更</button>
-              </div>
+             </div>
             <div class="mx-2">
-	        <img src="{{ Storage::disk('s3')->url($image->file_path) }}" style="width:100%;"　alt="建設現場の写真"/>
+	            <img src="{{ Storage::disk('s3')->url($image->file_path) }}" style="width:100%;"　alt="建設現場の写真"/>
 	        </div>
 	        </form>
-	            </div>
+	</div>
 </div>
 	        
 	        @endsection
