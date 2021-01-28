e12ef3ef5e44cc608e9862b76a9963ff6f9a9cc7
masakatsu.nomura <masakatsunomura26@gmail.com>
2021-01-28 11:21:09 +0000

    例外処理・トランザクション追加

--- app/Http/Controllers/ImageListController.php
+++ app/Http/Controllers/ImageListController.php
@@ -8,26 +8,34 @@ use App\UploadImage;
 
 use Illuminate\Support\Facades\Storage;
 
+use Illuminate\Support\Facades\DB;
+
 class ImageListController extends Controller
 {
     function show(){
-    	if (\Auth::check()) { // 認証済みの場合
-            // 認証済みユーザを取得
-            $user = \Auth::user();
+        // 認証済みユーザを取得
+        $user = \Auth::user();
 		//アップロードした画像を取得
 		$uploads = $user->uploadimages()->orderBy("id", "desc")->get();
 		// $uploads = UploadImage::orderBy("id", "desc")->get();
 		return view("image_list",[
 			"images" => $uploads
 		]);
-    	}
+    	
 	}
 	
 	function destroy($id){
 		$deletePictures = UploadImage::find($id);
 		$deleteName = $deletePictures->file_path;
-		Storage::disk('s3')->delete($deleteName);
+		
+		DB::beginTransaction();
 		$deletePictures->delete();
+		try {
+		Storage::disk('s3')->delete($deleteName);
+		DB::commit();
+		} catch (Exception $exception) {
+			DB::rollBack();
+		}
 		return redirect("/list");
 	}
 }
