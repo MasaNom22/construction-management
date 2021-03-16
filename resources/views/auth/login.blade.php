5cd1999b489f8a1cad959cc79e5a462f3b840733
masakatsu.nomura <masakatsunomura26@gmail.com>
2021-03-16 12:36:06 +0000

    ログイン画面修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,73 +3,41 @@
 @section('title', 'ログイン画面')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('ログイン') }}</div>
+  <div class="container">
+    <div class="row">
+      <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
+        <div class="card mt-3">
+          <div class="card-body text-center">
+            <h2 class="h3 card-title text-center mt-2">ログイン</h2>
+            
+            <div class="card-text">
+              <form method="POST" action="{{ route('login') }}">
+                @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('メールアドレス') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" placeholder="例)abc@gmail.com" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('パスワード') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('次からは入力を省略') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-success">
-                                    {{ __('ログイン') }}
-                                </button>
-                                <a class="btn btn-success" href="{{ route('login.guest') }}">ゲストログイン</a>
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                                <h5>ゲストログインボタンからお入りください</h5>
-                            </div>
-                        </div>
-                    </form>
+                <div class="md-form">
+                  <label for="email">メールアドレス</label>
+                  <input class="form-control" type="text" id="email" name="email" required value="{{ old('email') }}">
                 </div>
+
+                <div class="md-form">
+                  <label for="password">パスワード</label>
+                  <input class="form-control" type="password" id="password" name="password" required>
+                </div>
+ 
+                <input type="hidden" name="remember" id="remember" value="on">
+
+                <button class="btn aqua-gradient mt-2 mb-2" type="submit">ログイン</button>
+                <a class="btn aqua-gradient" href="{{ route('login.guest') }}">ゲストログイン</a>
+              </form>
+                <h5 class="mt-2 text-dark">ゲストログインボタンからお入りください</h5>
+              <div class="mt-0">
+                <a href="{{ route('signup.get') }}" class="card-text">ユーザー登録はこちら</a>
+              </div>
+
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 @endsection
